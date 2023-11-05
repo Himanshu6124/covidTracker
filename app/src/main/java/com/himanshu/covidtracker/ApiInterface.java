4663f6aee18a8364d1e07af68d896646540dffc2
Himanshu Pathsaria <himanshu6124@gmail.com>
2023-11-05 15:19:42 +0530

    Himanshu | adding screenshot and changed api endpoint

--- app/src/main/java/com/himanshu/covidtracker/ApiInterface.java
+++ app/src/main/java/com/himanshu/covidtracker/ApiInterface.java
@@ -6,7 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
-    String BASE_URL = "https://corona.lmao.ninja/v2/";
+    String BASE_URL = "https://disease.sh/v3/covid-19/";
 
     @GET("countries")
     Call<List<ModelClass>> getcountrydata();
